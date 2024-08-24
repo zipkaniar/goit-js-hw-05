0cf5fe3a8fa98bf97235f43e12c999533327cd5b
Murat Çolak <mo939614@gmail.com>
2024-08-24 13:01:14 +0300

    Edit quest.

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 const sortByDescendingFriendCount = (users) => {
-  return users.toSorted((a, b) => b.friends.length - a.friends.length);
+  return users.slice().sort((a, b) => b.friends.length - a.friends.length);
 };
 console.log(
   sortByDescendingFriendCount([
