afd41d0c8450f9d4bf10c9c9e2a7d117b8858035
Murat Çolak <mo939614@gmail.com>
2024-08-26 08:57:40 +0300

    task3 and 4 edit.

--- js/task-4.js
+++ js/task-4.js
@@ -3,7 +3,44 @@ const getTotalBalanceByGender = (users, gender) => {
     return user.gender === gender ? total + user.balance : total;
   }, 0);
 };
+const allUsersVersion2 = [
+  {
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811,
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821,
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793,
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278,
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951,
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498,
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764,
+  },
+];
 
-console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+console.log(getTotalBalanceByGender(allUsersVersion2, "male")); // 12053
 
-console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
+console.log(getTotalBalanceByGender(allUsersVersion2, "female")); // 8863
